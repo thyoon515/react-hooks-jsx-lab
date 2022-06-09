c6979c96b045cacb4050db958778297dd98ec438
thyoon515 <thyoon515@gmail.com>
2022-06-08 23:47:02 -0400

    commit tree 7471d29202a47e3e4d47107ffd657d30319f7c4d

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const homeStyle = {color: "firebrick"}
+  return (
+  <div id="home">
+    <h1 style={homeStyle}>
+      {name} is a Web Developer from {city}
+    </h1>
+  </div>
+  );
 }
 
 export default Home;
